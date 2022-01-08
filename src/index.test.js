2506015d9643fc6efca05e3f58189ef1ca30cf2d
Bryan Lloyd <bryan.lloyd@wilder.org>
2022-01-08 08:41:58 -0600

    Update tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DataBook from './components/DataBook';
-import { QUESTIONS, TOPICS, RESULTTYPES } from './docs/data';
+import { QUESTIONS, TOPICS, RESULTTYPES, DATAYEARS } from './docs/data'; // docs/data.js
 import CSV2018 from './docs/csv-2018'; // docs/csv-2018.json
 import CSV2021 from './docs/csv-2021'; // docs/csv-2021.json
 
@@ -14,6 +14,7 @@ it('renders without crashing', () => {
       questions={QUESTIONS}
       topics={TOPICS}
       resulttypes={RESULTTYPES}
+      datayears={DATAYEARS}
       csv2018={CSV2018}
       csv2021={CSV2021}
 />, div);
